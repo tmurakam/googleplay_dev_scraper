f6d20ba538d66f03e3d941bf70cc3b7ec728e385
Takuya Murakami <tmurakam@tmurakam.org>
2012-12-13 18:09:46 +0900

    fix spec

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,9 +1,33 @@
 # -*- coding: utf-8 -*-
 require 'spec_helper'
 
+class PageMock
+  def body
+    return "BODY"
+  end
+end
+
 describe GooglePlayScraper::Scraper do
   before do
     @scraper = GooglePlayScraper::Scraper.new
+
+    @dev_acc = "1234567890"
+    @scraper.dev_acc = @dev_acc
+
+    # replace method!
+    def @scraper.try_get(url)
+      setup
+      @accessed_url = url
+
+      def @agent.page
+        page = PageMock.new
+        return page
+      end
+    end
+
+    def @scraper.accessed_url
+      @accessed_url
+    end
   end
 
   context "Setup" do
@@ -25,6 +49,12 @@ describe GooglePlayScraper::Scraper do
     end
   end
 
+  context "get sales report" do
+    it "normal access" do
+      @scraper.get_sales_report(2012, 11)
+      @scraper.accessed_url.should == "https://play.google.com/apps/publish/salesreport/download?report_date=2012_11&report_type=payout_report&dev_acc=#{@dev_acc}"
+    end
+  end
   
 end
 
