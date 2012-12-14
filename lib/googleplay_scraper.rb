8a342ad3159c15c72fce5e7387c437676138091d
Takuya Murakami <tmurakam@tmurakam.org>
2012-12-14 18:09:40 +0900

    refactor

--- lib/googleplay_scraper.rb
+++ lib/googleplay_scraper.rb
@@ -2,7 +2,3 @@ require "googleplay_scraper/version"
 require "googleplay_scraper/scraper_base"
 require "googleplay_scraper/scraper_config"
 require "googleplay_scraper/scraper"
-
-module GooglePlayScraper
-  # Your code goes here...
-end
